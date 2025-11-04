318f8a357d4afea324923b63aa7d4841d7de656c
martiijs <martijs.escola@gmail.com>
2025-11-04 15:00:00 +0100

    canvis

--- TresEnRatlla/TresEnRatlla.cpp
+++ TresEnRatlla/TresEnRatlla.cpp
@@ -2,10 +2,117 @@
 //
 
 #include <iostream>
+#include <cstdlib>  
+using namespace std;
+
+string pos1;
+string pos2;
+string pos3;
+string pos4;
+string pos5;
+string pos6;
+string pos7;
+string pos8;
+string pos9;
+int torn;
+int posicio;
+string tipusFitxa;
+
+bool agafada;
+bool escollint;
+bool jugant;
+void Jugada();
+void printTauler();
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    torn = 1;
+    jugant = true;
+    while (jugant == true) {
+        cout << "1";
+        Jugada();
+        //system("cls");
+        printTauler();
+    }
+}
+
+void printTauler() {
+
+    cout << "____________" << endl;
+    cout << pos1;
+    cout << " | ";
+    cout << pos2;
+    cout << " | ";
+    cout << pos3;
+    cout << " | " << endl;
+    cout << "____________" << endl;
+    cout << pos4;
+    cout << " | ";
+    cout << pos5;
+    cout << " | ";
+    cout << pos6;
+    cout << " | " << endl;
+    cout << "____________" << endl;
+    cout << pos7;
+    cout << " | ";
+    cout << pos8;
+    cout << " | ";
+    cout << pos9;
+    cout << " | " << endl;
+    cout << "____________" << endl;
+}
+
+void Jugada() {
+
+    cout << "1.1";
+    
+    if (torn == 1) {
+        tipusFitxa = "x";
+        escollint = true;
+    }
+
+    if (torn == 2) {
+        tipusFitxa = "o";
+        escollint = true;
+    }
+
+    while (escollint == true) {
+        cout << "Jugada: ";
+        cin >> posicio;
+        switch (posicio) {
+        case 1: 
+            if (agafada == false) { 
+                pos1 = tipusFitxa;  
+                escollint = false; 
+                if (torn == 1) {
+                    torn = 2;
+                } 
+                if (torn == 2) { 
+                    torn = 1; 
+                } 
+            }
+            else {
+                cout << "escull una altre opció";
+            }
+            break;
+        case 2: if (agafada == false) { pos2 = tipusFitxa;  escollint = false; if (torn == 1) { torn = 2; } if (torn == 2) { torn = 1; } }
+              else cout << "escull una altre opció"; break;
+        case 3: if (agafada == false) { pos3 = tipusFitxa;  escollint = false; if (torn == 1) { torn = 2; } if (torn == 2) { torn = 1; }}
+              else cout << "escull una altre opció"; break;
+        case 4: if (agafada == false) { pos4 = tipusFitxa;  escollint = false; if (torn == 1) { torn = 2; } if (torn == 2) { torn = 1; }}
+              else cout << "escull una altre opció"; break;
+        case 5: if (agafada == false) { pos5 = tipusFitxa;  escollint = false; if (torn == 1) { torn = 2; } if (torn == 2) { torn = 1; }}
+              else cout << "escull una altre opció";  break;
+        case 6: if (agafada == false) { pos6 = tipusFitxa;  escollint = false; if (torn == 1) { torn = 2; } if (torn == 2) { torn = 1; }}
+              else cout << "escull una altre opció";  break;
+        case 7: if (agafada == false) { pos7 = tipusFitxa;  escollint = false; if (torn == 1) { torn = 2; } if (torn == 2) { torn = 1; }}
+              else cout << "escull una altre opció";  break;
+        case 8: if (agafada == false) { pos8 = tipusFitxa;  escollint = false; if (torn == 1) { torn = 2; } if (torn == 2) { torn = 1; }}
+              else cout << "escull una altre opció";  break;
+        case 9: if (agafada == false) { pos9 = tipusFitxa;  escollint = false; if (torn == 1) { torn = 2; } if (torn == 2) { torn = 1; }}
+              else cout << "escull una altre opció";  break;
+        }
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
